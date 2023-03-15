4d79a73a218974f1d9dd62f8279a282884a3fca4
brianguerra <brianm.guerrag@outlook.es>
2023-03-15 13:38:33 -0600

    web-headers

---
+++ src/main/java/org/bguerra/apiserclet/webapp/headers/services/ProductoService.java
@@ -0,0 +1,9 @@
+package org.bguerra.apiserclet.webapp.headers.services;
+
+import org.bguerra.apiserclet.webapp.headers.models.Producto;
+
+import java.util.List;
+
+public interface ProductoService {
+    List<Producto> listar();
+}
